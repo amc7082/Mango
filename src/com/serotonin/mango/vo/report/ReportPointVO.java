672c1fd99b58993134c8fe19081a7c187ba8ff54
HKing2802 <50064702+HKing2802@users.noreply.github.com>
2022-11-28 18:58:52 -0500

    Updated class to handle new report information

--- src/com/serotonin/mango/vo/report/ReportPointVO.java
+++ src/com/serotonin/mango/vo/report/ReportPointVO.java
@@ -1,16 +1,19 @@
 package com.serotonin.mango.vo.report;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 
+import com.serotonin.ShouldNeverHappenException;
 import com.serotonin.util.SerializationHelper;
 
 public class ReportPointVO implements Serializable {
     private int pointId;
     private String colour;
     private boolean consolidatedChart;
+    private String type;
+    private String title;
+    private String xLabel;
+    private String yLabel;
+    private String referenceLine;
 
     public int getPointId() {
         return pointId;
@@ -36,6 +39,46 @@ public class ReportPointVO implements Serializable {
         this.consolidatedChart = consolidatedChart;
     }
 
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getXLabel() {
+        return this.xLabel;
+    }
+
+    public void setXLabel(String xLabel) {
+        this.xLabel = xLabel;
+    }
+
+    public String getYLabel() {
+        return this.yLabel;
+    }
+
+    public void setYLabel(String yLabel) {
+        this.yLabel = yLabel;
+    }
+
+    public String getReferenceLine() {
+        return this.referenceLine;
+    }
+
+    public void setReferenceLine(String referenceLine) {
+        this.referenceLine = referenceLine;
+    }
+
     //
     //
     // Serialization
@@ -49,6 +92,11 @@ public class ReportPointVO implements Serializable {
         out.writeInt(pointId);
         SerializationHelper.writeSafeUTF(out, colour);
         out.writeBoolean(consolidatedChart);
+        SerializationHelper.writeSafeUTF(out, type);
+        SerializationHelper.writeSafeUTF(out, title);
+        SerializationHelper.writeSafeUTF(out, xLabel);
+        SerializationHelper.writeSafeUTF(out, yLabel);
+        SerializationHelper.writeSafeUTF(out, referenceLine);
     }
 
     private void readObject(ObjectInputStream in) throws IOException {
@@ -60,10 +108,20 @@ public class ReportPointVO implements Serializable {
             colour = SerializationHelper.readSafeUTF(in);
             consolidatedChart = true;
         }
-        else if (ver == 2) {
+        else if (ver == 2 || ver == 3) {
             pointId = in.readInt();
             colour = SerializationHelper.readSafeUTF(in);
             consolidatedChart = in.readBoolean();
+
+            try {
+                type = SerializationHelper.readSafeUTF(in);
+                title = SerializationHelper.readSafeUTF(in);
+                xLabel = SerializationHelper.readSafeUTF(in);
+                yLabel = SerializationHelper.readSafeUTF(in);
+                referenceLine = SerializationHelper.readSafeUTF(in);
+            } catch (EOFException e) {
+                // squash
+            }
         }
     }
 }
